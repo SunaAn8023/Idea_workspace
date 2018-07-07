637a0471fdcbc22c31ee83c407f38fc5493be059
SunaAn <1005114303@qq.com>
2018-07-07 19:56:05 +0800

    Initial commit

---
+++ src/main/java/com/example/BinTree.java
@@ -0,0 +1,70 @@
+package com.example;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+//二叉树层次遍历
+public class BinTree {
+
+    private int data;
+    private BinTree lchild;//左孩子
+    private BinTree rchild;//右孩子
+
+    private BinTree (int c) {
+       data=c;
+    }
+
+    //层次遍历方法
+    public static  void cenci(BinTree node){
+        if (node==null){
+            return;
+        }
+        Queue<BinTree> queue=new ArrayDeque<BinTree>();
+        //offer&&poll优于add&&remove是因为它们的返回值可以判断成功与否，而不抛出异常
+
+        //1.将根节点放入队列
+        queue.offer(node);
+        //2.若队列非空，将一个结点出队列，并访问该节点
+        while(!queue.isEmpty()){
+            node=queue.poll();
+            System.out.println(node.data);
+
+            //3.若该节点的左子树为非空，则将该节点的右孩子节点入队列
+            if (node.lchild!=null){
+                queue.offer(node.lchild);
+            }
+            //4.若该节点的右子树为非空，则将该节点的右孩子节点入队列
+            if (node.rchild!=null){
+                queue.offer(node.rchild);
+            }
+        }
+
+    }
+
+
+
+    public static  void  main(String[] args){
+        BinTree t1=new BinTree(12);
+        BinTree t2=new BinTree(15);
+        BinTree t3=new BinTree(8);
+        BinTree t4=new BinTree(11);
+        BinTree t5=new BinTree(15);
+        BinTree t6=new BinTree(17);
+        BinTree t7=new BinTree(19);
+        BinTree t8=new BinTree(20);
+
+        //构成二叉树 t1\t2\t3
+        t1.lchild=t2;
+        t1.rchild=t3;
+
+        t2.lchild=t4;
+        t2.rchild=t5;
+
+        t3.lchild=t6;
+        t3.rchild=t7;
+
+       //调用二叉树层次遍历方法
+        BinTree.cenci(t1);
+
+    }
+}
