f84f8782903f2aa83925abaf7e91034bd9e43c10
zwy <wenyang.chou@foxmail.com>
2018-07-21 19:22:32 +0800

    测试

--- src/main/java/com/example/pattern/stringMatch/kmp/Kmp.java
+++ src/main/java/com/example/pattern/stringMatch/kmp/Kmp.java
@@ -69,7 +69,7 @@ public class Kmp {
 
 
     public static void main(String[] args) {
-        int get = index("abcdef","cd");
+        int get = index("abcdef","cde");
         System.out.println(get);
     }
 }
